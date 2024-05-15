4ca39967a1e11d16b91597096ab2754b8f9377e2
karimsh2001 <karimsh2001@gmail.com>
2024-05-15 19:13:43 -0400

    front y back 1

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 $serverName = "WO\SQLEXPRESS";
 $connectionOptions = array(
-    "Database" => "db_name",
-    "Uid" => "WO\Karim",
-    "PWD" => "password"
+    "Database" => "ComprasDB",
+    "Uid" => "sa",
+    "PWD" => "1234"
 );
 
 //Establishes the connection
@@ -13,14 +13,40 @@ if($conn === false) {
     die(print_r(sqlsrv_errors(), true));
 }
 
-$tsql= "SELECT * FROM table_name";
-$getResults= sqlsrv_query($conn, $tsql);
-if ($getResults == FALSE) {
-    die(print_r(sqlsrv_errors(), true));
-}
+// Define your routes
+$routes = [
+    '/getlineas' => function() use ($conn) {
+        $tsql= "SELECT * FROM Lineas ORDER BY CodLinea";
+        $getResults= sqlsrv_query($conn, $tsql);
+        if ($getResults == FALSE) {
+            die(print_r(sqlsrv_errors(), true));
+        }
 
-while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-    echo $row['column_name'].", ";
+        $data = array();
+        while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+            $data[] = $row;
+        }
+        sqlsrv_free_stmt($getResults);
+
+        // Add the CORS headers
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+        header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept, Origin, Authorization');
+
+        header('Content-Type: application/json');
+        echo json_encode($data);
+    },
+    // Add more routes here...
+];
+
+// Get the path from the URL
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+// If the path matches a route, run the associated function
+if (isset($routes[$path])) {
+    $routes[$path]();
+} else {
+    http_response_code(404);
+    echo 'Not found';
 }
-sqlsrv_free_stmt($getResults);
 ?>
